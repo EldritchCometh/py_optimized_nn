e44de962dec0eceadad1b301d6eb73a9d1c90c3c
admin01 <abanzhof@gmail.com>
2023-05-17 18:08:58 -0400

    idk I must have changed something. it still works though

--- network.py
+++ network.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class Layer:
-    
+
     def __init__(self):
         self.input = None
         self.output = None
@@ -28,8 +28,16 @@ class Activation(Layer):
         return np.multiply(output_gradient, self.activation_prime(self.input))
 
 
+class Tanh(Activation):
+
+    def __init__(self):
+        def tanh(x): return np.tanh(x)
+        def tanh_prime(x): return 1 - np.tanh(x) ** 2
+        super().__init__(tanh, tanh_prime)
+
+
 class Dense(Layer):
-    
+
     def __init__(self, input_size, output_size):
         self.weights = np.random.randn(output_size, input_size)
         self.bias = np.random.randn(output_size, 1)
@@ -37,23 +45,15 @@ class Dense(Layer):
     def forward(self, input):
         self.input = input
         return np.dot(self.weights, self.input) + self.bias
-    
+
     def backward(self, output_gradient, learning_rate):
         weights_gradient = np.dot(output_gradient, self.input.T)
         self.weights -= learning_rate * weights_gradient
         self.bias -= learning_rate * output_gradient
         return np.dot(self.weights.T, output_gradient)
-        
-
-class Tanh(Activation):
-
-    def __init__(self):
-        tanh = lambda x: np.tanh(x)
-        tanh_prime = lambda x: 1 - np.tanh(x) ** 2
-        super().__init__(tanh, tanh_prime)
 
 
-def mse(y_true, y_pred):    
+def mse(y_true, y_pred):
     return np.mean(np.power(y_true - y_pred, 2))
 
 
@@ -67,7 +67,6 @@ print(dense.forward([0.5, 0.5, 0.5]))
 X = np.reshape([[0, 0], [0, 1], [1, 0], [1, 1]], (4, 2, 1))
 Y = np.reshape([[0], [1], [1], [0]], (4, 1, 1))
 
-
 network = [
     Dense(2, 3),
     Tanh(),
@@ -75,8 +74,7 @@ network = [
     Tanh(),
 ]
 
-
-epochs = 10000
+epochs = 1000
 learning_rate = 0.1
 
 for e in range(epochs):
