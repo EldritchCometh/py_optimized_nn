cc919e7c2ea0b7a3ea655314e56085d749e9d3a7
admin01 <abanzhof@gmail.com>
2023-05-17 20:40:15 -0400

    just working on making this thing

--- network.py
+++ network.py
@@ -1,24 +1,17 @@
 import numpy as np
 
 
-class Layer:
-    
+class Activation:
+
     def __init__(self):
         self.input = None
         self.output = None
+        self.activation = None
+        self.activation_prime = None
 
-    def forward(self, input):
-        pass
-
-    def backward(self, output_gradient, learning_rate):
-        pass
-
-
-class Activation(Layer):
-
-    def __init__(self, activation, activation_prime):
-        self.activation = activation
-        self.activation_prime = activation_prime
+    def set_tanh(self):
+        self.activation = lambda x: np.tanh(x)
+        self.activation_prime = lambda x: 1 - np.tanh(x) ** 2
 
     def forward(self, input):
         self.input = input
@@ -28,66 +21,84 @@ class Activation(Layer):
         return np.multiply(output_gradient, self.activation_prime(self.input))
 
 
-class Dense(Layer):
-    
+class Dense:
+
     def __init__(self, input_size, output_size):
+        self.input = None
+        self.output = None
         self.weights = np.random.randn(output_size, input_size)
         self.bias = np.random.randn(output_size, 1)
 
     def forward(self, input):
         self.input = input
         return np.dot(self.weights, self.input) + self.bias
-    
+
     def backward(self, output_gradient, learning_rate):
         weights_gradient = np.dot(output_gradient, self.input.T)
         self.weights -= learning_rate * weights_gradient
         self.bias -= learning_rate * output_gradient
         return np.dot(self.weights.T, output_gradient)
-        
 
-class Tanh(Activation):
+
+class Network:
 
     def __init__(self):
-        tanh = lambda x: np.tanh(x)
-        tanh_prime = lambda x: 1 - np.tanh(x) ** 2
-        super().__init__(tanh, tanh_prime)
+        self.layers = []
+
+    def add_layer(self, layer):
+        self.layers.append(layer)
+
+    def forward(self, input):
+        output = None
+        for layer in self.layers:
+            output = layer.forward(input)
+            input = output
+        return output
 
 
-def mse(y_true, y_pred):    
-    return np.mean(np.power(y_true - y_pred, 2))
+class Trainer:
+
+    def __init__(self, network, training_samples):
+        self.nn = network
+        self.samples = training_samples
+
+    @staticmethod
+    def mse(y_true, y_pred):
+        return np.mean(np.power(y_true - y_pred, 2))
+
+    @staticmethod
+    def mse_prime(y_true, y_pred):
+        return 2 * (y_pred - y_true) / np.size(y_true)
+
+    def backward(self, gradient, learning_rate):
+        for layer in reversed(self.nn):
+            gradient = layer.backward(gradient, learning_rate)
+
+    def train(self, learning_rate, epochs):
+        for e in range(epochs):
+            error = 0
+            samples = self.samples
+            np.random.shuffle(samples)
+            for sample in samples:
+                features, targets = sample
+                output = self.nn.forward(features)
+                error += self.mse(targets, output)
+                output_gradient = self.mse_prime(targets, output)
+                self.backward(output_gradient, learning_rate)
+            error /= len(X)
+            print('%d/%d, error=%f' % (e + 1, epochs, error))
 
 
-def mse_prime(y_true, y_pred):
-    return 2 * (y_pred - y_true) / np.size(y_true)
+if __name__ == "__main__":
 
+    X = np.array([[[0], [0]], [[0], [1]], [[1], [0]], [[1], [1]]])
+    Y = np.array([[[0]], [[1]], [[1]], [[0]]])
+    training_samples = np.concatenate((X, Y), axis=2)
 
-dense = Dense(3, 3)
-print(dense.forward([0.5, 0.5, 0.5]))
+    nn = Network()
+    nn.add_layer(Dense(2, 3))
+    nn.add_layer(Activation().set_tanh())
+    nn.add_layer(Dense(3, 1))
+    nn.add_layer(Activation().set_tanh())
 
-X = np.reshape([[0, 0], [0, 1], [1, 0], [1, 1]], (4, 2, 1))
-Y = np.reshape([[0], [1], [1], [0]], (4, 1, 1))
-
-
-network = [
-    Dense(2, 3),
-    Tanh(),
-    Dense(3, 1),
-    Tanh(),
-]
-
-
-epochs = 10000
-learning_rate = 0.1
-
-for e in range(epochs):
-    error = 0
-    for x, y in zip(X, Y):
-        output = x
-        for layer in network:
-            output = layer.forward(output)
-        error += mse(y, output)
-        grad = mse_prime(y, output)
-        for layer in reversed(network):
-            grad = layer.backward(grad, learning_rate)
-    error /= len(X)
-    print('%d/%d, error=%f' % (e + 1, epochs, error))
+    Trainer(nn, training_samples).train(0.1, 10000)
